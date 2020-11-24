6cf04b87e7fa3f6d5b4eb20f84e93ca100438a2d
Will Robinson <3.will.robinson.a@gmail.com>
2020-11-24 15:55:43 -0600

    Updated sql query.

--- Jax Housing Analysis/2.JoinAndQuery.sql
+++ Jax Housing Analysis/2.JoinAndQuery.sql
@@ -7,18 +7,18 @@ SELECT
 		/* Owner table query */
 		owner_1, owner_2, owner_3,
 		/* Parcel table query */
-		CASE WHEN mailing_2 NOT NULL THEN (TRIM(mailing_1)||' '||TRIM(mailing_2)) 
-			ELSE TRIM(mailing_1) END AS mailing_address,
+		CASE WHEN mailing_address_2 NOT NULL THEN (TRIM(mailing_address_1)||' '||TRIM(mailing_address_2)) 
+			ELSE TRIM(mailing_address_1) END AS mailing_address,
 		/* Two properties in the database did not list mailing_city. In both cases, the owner resides in Jacksonville. */
-		CASE WHEN mailing_city NOT NULL THEN TRIM(mailing_city)
+		CASE WHEN city NOT NULL THEN TRIM(city)
 			ELSE 'JACKSONVILLE' END AS mailing_city, 
-		IFNULL(mailing_state, 'FOREIGN COUNTRY') mailing_state, property_use, 
-		IFNULL(subdivision, 'NOT LISTED') subdivision, neighborhood, val_method, 
+		IFNULL(state, 'FOREIGN COUNTRY') AS mailing_state, property_use, 
+		IFNULL(subdivision_name, 'NOT LISTED') AS subdivision, neighborhood, value_method, 
 		CASE WHEN cap_base_yr = 1 THEN 0
 			ELSE cap_base_yr END AS cap_base_yr, 
-		market_val, assessed_val, building_val, just_val, school_taxable, county_taxable, sjrwnd_taxable, 
-		taxing_district, square_feet,
-		/* Characteristic table query */
+		market_value, assessed_value, building_value, just_value, school_taxable, county_taxable, sjrwmd_taxable, 
+		tax_district, lot_sf,
+		/* Character table query */
 		char_descr,
 		canal,
 		golf,
@@ -89,13 +89,13 @@ USING (RE)
 /* Utility table query */
 LEFT JOIN
 (SELECT  RE, building,
-		SUM(CASE WHEN structure_descr = 'Stories' THEN unit_count
+		SUM(CASE WHEN structure_descr = 'Stories' THEN units
 			ELSE NULL END) AS stories,
-		SUM(CASE WHEN structure_descr = 'Bedrooms' THEN unit_count
+		SUM(CASE WHEN structure_descr = 'Bedrooms' THEN units
 			ELSE NULL END) AS bedrooms,
-		SUM(CASE WHEN structure_descr = 'Baths' THEN unit_count
+		SUM(CASE WHEN structure_descr = 'Baths' THEN units
 			ELSE NULL END) AS baths,
-		SUM(CASE WHEN structure_descr LIKE 'Rooms%Units' THEN unit_count
+		SUM(CASE WHEN structure_descr LIKE 'Rooms%Units' THEN units
 			ELSE NULL END) AS rooms
 -- 		SUM(CASE WHEN structure_descr = 'Restrooms' THEN unit_count
 -- 			ELSE NULL END) AS restrooms,
@@ -104,7 +104,7 @@ LEFT JOIN
 FROM Utility
 GROUP BY RE, building) AS room_query
 USING (RE, building)
-/* Characteristic table query */
+/* Character table query */
 LEFT JOIN
 (SELECT  RE,
 		MAX(CASE WHEN char_descr = 'ARLINGTON RIVER' THEN char_descr
@@ -148,8 +148,8 @@ LEFT JOIN
 FROM
 (SELECT RE
 FROM Parcel
-WHERE property_use = 100)
-LEFT JOIN Characteristic
+WHERE property_use = '0100')
+LEFT JOIN Character
 USING (RE)
 GROUP BY RE) AS proximity_sub
 USING (RE) 
@@ -182,7 +182,7 @@ LEFT JOIN
 FROM 
 (SELECT RE
 FROM Parcel
-WHERE property_use = 100)
+WHERE property_use = '0100')
 LEFT JOIN Common
 USING (RE)
 GROUP BY RE) AS land_use_query
@@ -219,76 +219,76 @@ USING (RE)
 LEFT JOIN
 (SELECT  RE,
 		IFNULL(building,0) AS building,
-		MAX(CASE WHEN descr LIKE 'Boat%' THEN units
+		MAX(CASE WHEN ft_descr LIKE 'Boat%' THEN units
 			ELSE NULL END) AS boat_cv,
-		MAX(CASE WHEN descr LIKE 'Boat%' THEN ppu
+		MAX(CASE WHEN ft_descr LIKE 'Boat%' THEN ppu
 			ELSE NULL END) AS boatcv_ppu,
-		MAX(CASE WHEN descr LIKE 'Boat%' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr LIKE 'Boat%' THEN actual_yr_built
 			ELSE NULL END) AS boatcv_yr,
-		MAX(CASE WHEN descr LIKE 'Carport%' THEN units
+		MAX(CASE WHEN ft_descr LIKE 'Carport%' THEN units
 			ELSE NULL END) AS carport,
-		MAX(CASE WHEN descr LIKE 'Carport%' THEN ppu
+		MAX(CASE WHEN ft_descr LIKE 'Carport%' THEN ppu
 			ELSE NULL END) AS carport_ppu,
-		MAX(CASE WHEN descr LIKE 'Carport%' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr LIKE 'Carport%' THEN actual_yr_built
 			ELSE NULL END) AS carport_yr,
-		MAX(CASE WHEN descr = 'Cov Patio' THEN units
+		MAX(CASE WHEN ft_descr = 'Cov Patio' THEN units
 			ELSE NULL END) AS cov_patio,
-		MAX(CASE WHEN descr = 'Cov Patio' THEN ppu
+		MAX(CASE WHEN ft_descr = 'Cov Patio' THEN ppu
 			ELSE NULL END) AS covpatio_ppu,
-		MAX(CASE WHEN descr = 'Cov Patio' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr = 'Cov Patio' THEN actual_yr_built
 			ELSE NULL END) AS covpatio_yr,
-		MAX(CASE WHEN descr = 'Deck Wd' THEN units
+		MAX(CASE WHEN ft_descr = 'Deck Wd' THEN units
 			ELSE NULL END) AS deck,
-		MAX(CASE WHEN descr = 'Deck Wd' THEN ppu
+		MAX(CASE WHEN ft_descr = 'Deck Wd' THEN ppu
 			ELSE NULL END) AS deck_ppu,
-		MAX(CASE WHEN descr = 'Deck Wd' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr = 'Deck Wd' THEN actual_yr_built
 			ELSE NULL END) AS deck_yr,
-		MAX(CASE WHEN descr LIKE 'Dock%' THEN units
+		MAX(CASE WHEN ft_descr LIKE 'Dock%' THEN units
 			ELSE NULL END) AS dock,
-		MAX(CASE WHEN descr LIKE 'Dock%' THEN ppu
+		MAX(CASE WHEN ft_descr LIKE 'Dock%' THEN ppu
 			ELSE NULL END) AS dock_ppu,
-		MAX(CASE WHEN descr LIKE 'Dock%' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr LIKE 'Dock%' THEN actual_yr_built
 			ELSE NULL END) AS dock_yr,
-		MAX(CASE WHEN descr LIKE 'Firep%' THEN units
+		MAX(CASE WHEN ft_descr LIKE 'Firep%' THEN units
 			ELSE NULL END) AS fireplace,
-		MAX(CASE WHEN descr LIKE 'Firep%' THEN ppu
+		MAX(CASE WHEN ft_descr LIKE 'Firep%' THEN ppu
 			ELSE NULL END) AS fireplace_ppu,
-		MAX(CASE WHEN descr LIKE 'Firep%' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr LIKE 'Firep%' THEN actual_yr_built
 			ELSE NULL END) AS fireplace_yr,
-		MAX(CASE WHEN descr = 'Pool' THEN units
+		MAX(CASE WHEN ft_descr = 'Pool' THEN units
 			ELSE NULL END) AS pool,
-		MAX(CASE WHEN descr = 'Pool' THEN ppu
+		MAX(CASE WHEN ft_descr = 'Pool' THEN ppu
 			ELSE NULL END) AS pool_ppu,
-		MAX(CASE WHEN descr = 'Pool' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr = 'Pool' THEN actual_yr_built
 			ELSE NULL END) AS pool_yr,
-		MAX(CASE WHEN descr = 'Scr Porch' THEN units
+		MAX(CASE WHEN ft_descr = 'Scr Porch' THEN units
 			ELSE NULL END) AS scr_porch,
-		MAX(CASE WHEN descr = 'Scr Porch' THEN ppu
+		MAX(CASE WHEN ft_descr = 'Scr Porch' THEN ppu
 			ELSE NULL END) AS scrporch_ppu,
-		MAX(CASE WHEN descr = 'Scr Porch' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr = 'Scr Porch' THEN actual_yr_built
 			ELSE NULL END) AS scrporch_yr,
-		MAX(CASE WHEN descr LIKE 'Screen%' THEN units
+		MAX(CASE WHEN ft_descr LIKE 'Screen%' THEN units
 			ELSE NULL END) AS screen_encl,
-		MAX(CASE WHEN descr LIKE 'Screen%' THEN ppu
+		MAX(CASE WHEN ft_descr LIKE 'Screen%' THEN ppu
 			ELSE NULL END) AS screenencl_ppu,
-		MAX(CASE WHEN descr LIKE 'Screen%' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr LIKE 'Screen%' THEN actual_yr_built
 			ELSE NULL END) AS screenencl_yr,
-		MAX(CASE WHEN descr LIKE 'Shed%' THEN units
+		MAX(CASE WHEN ft_descr LIKE 'Shed%' THEN units
 			ELSE NULL END) AS shed,
-		MAX(CASE WHEN descr LIKE 'Shed%' THEN ppu
+		MAX(CASE WHEN ft_descr LIKE 'Shed%' THEN ppu
 			ELSE NULL END) AS shed_ppu,
-		MAX(CASE WHEN descr LIKE 'Shed%' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr LIKE 'Shed%' THEN actual_yr_built
 			ELSE NULL END) AS shed_yr,
-		MAX(CASE WHEN descr = 'Sun Room' THEN units
+		MAX(CASE WHEN ft_descr = 'Sun Room' THEN units
 			ELSE NULL END) AS sun_rm,
-		MAX(CASE WHEN descr = 'Sun Room' THEN ppu
+		MAX(CASE WHEN ft_descr = 'Sun Room' THEN ppu
 			ELSE NULL END) AS sunrm_ppu,
-		MAX(CASE WHEN descr = 'Sun Room' THEN actual_yr_built
+		MAX(CASE WHEN ft_descr = 'Sun Room' THEN actual_yr_built
 			ELSE NULL END) AS sunrm_yr
 FROM
 (SELECT RE
 FROM Parcel
-WHERE property_use = 100)
+WHERE property_use = '0100')
 LEFT JOIN Feature
 USING (RE)
 GROUP BY RE, building) AS feature_subquery
@@ -297,26 +297,26 @@ USING (RE, building)
 LEFT JOIN
 (SELECT  RE,
 		IFNULL(building,0) AS building,
-		SUM(CASE WHEN sub_structure_descr = 'Addition' THEN 1
+		SUM(CASE WHEN substructure_descr = 'Addition' THEN 1
 			ELSE NULL END) AS additions,
-		SUM(CASE WHEN sub_structure_descr = 'Addition' THEN effec_area
+		SUM(CASE WHEN substructure_descr = 'Addition' THEN effec_area
 			ELSE NULL END) AS additions_effec_area,
-		SUM(CASE WHEN sub_structure_descr = 'Balcony' THEN effec_area
+		SUM(CASE WHEN substructure_descr = 'Balcony' THEN effec_area
 			ELSE NULL END) AS balcony_effec_area,
-		SUM(CASE WHEN sub_structure_descr LIKE '%Garage%' THEN effec_area
+		SUM(CASE WHEN substructure_descr LIKE '%Garage%' THEN effec_area
 			ELSE NULL END) AS garage_effec_area,
-		SUM(CASE WHEN sub_structure_descr = 'Finished Storage' THEN effec_area
+		SUM(CASE WHEN substructure_descr = 'Finished Storage' THEN effec_area
 			ELSE NULL END) AS fin_storage_effec_area,
-		SUM(CASE WHEN sub_structure_descr = 'Unfinished Storage' THEN effec_area
+		SUM(CASE WHEN substructure_descr = 'Unfinished Storage' THEN effec_area
 			ELSE NULL END) AS unfin_storage_effec_area
 FROM
 (SELECT RE
 FROM Parcel
-WHERE property_use = 100)
+WHERE property_use = '0100')
 LEFT JOIN Subarea
 USING (RE)
 GROUP BY RE, building) AS subarea_query
 USING (RE, building)
 )
 /* Single-family properties and qualified sales */
-WHERE property_use = 100 AND ((status = 'Qualified' AND improved = 'I') OR status IS NULL)
+WHERE property_use = '0100' AND ((status = 'Qualified' AND improved = 'I') OR status IS NULL)
